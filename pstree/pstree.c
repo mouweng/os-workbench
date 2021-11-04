822b1de9ef72f4fc29e220c2736d540f944975a0
tracer-nju <tracer@nju.edu.cn>
2021-11-04 14:18:35 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  14:18:35 up 118 days, 19:07,  0 users,  load average: 0.02, 0.05, 0.05

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -2,11 +2,16 @@
 #include <assert.h>
 
 int main(int argc, char *argv[]) {
+  assert(!argv[argc]);
+  if (argc != 2) {
+    printf("argc = %d, Argument Error!\n", argc);
+    return 0;
+  }
   for (int i = 0; i < argc; i++) {
     assert(argv[i]);
-    printf("argv[%d] = %s\n", i, argv[i]);
+    if (i == 1) {
+      printf("argv[%d] = %s\n", i, argv[i]);
+    }
   }
-  assert(!argv[argc]);
-  printf("argc = %d\n", argc);
   return 0;
 }
