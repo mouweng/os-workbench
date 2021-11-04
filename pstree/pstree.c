c1290c8166b15bfa9c7c9d703f5b203b56575d6e
tracer-nju <tracer@nju.edu.cn>
2021-11-04 14:28:31 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  14:28:31 up 118 days, 19:17,  0 users,  load average: 0.00, 0.01, 0.05

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -9,11 +9,11 @@ int main(int argc, char *argv[]) {
   }
   assert(argv[1]);
   // printf("argv = %s\n", argv[1]);
-  if (argv[1] == "-V" || argv[1] == "--version") {
+  if (*argv[1] == "-V" || *argv[1] == "--version") {
     printf("version\n");
-  } else if (argv[1] == "-n" || argv[1] == "--numeric-sort") {
+  } else if (*argv[1] == "-n" || *argv[1] == "--numeric-sort") {
     printf("numeric-sort\n");
-  } else if (argv[1] == "-p" || argv[1] == "--show-pids") {
+  } else if (*argv[1] == "-p" || *argv[1] == "--show-pids") {
     printf("show-pids\n");
   } else {
     printf("There Is No Such Argument Named %s\n", argv[1]);
