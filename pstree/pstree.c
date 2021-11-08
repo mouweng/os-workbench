a06c8ac63fb2b49483443a5c203d0becad7c495f
tracer-nju <tracer@nju.edu.cn>
2021-11-08 15:00:17 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  15:00:17 up 122 days, 19:49,  0 users,  load average: 0.02, 0.15, 0.18

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -11,9 +11,15 @@
 char default_path[1024] = "/proc/";
 
 int main(int argc, char *argv[]) {
+    int count = 0;
     char dir[1024];
     struct dirent **namelist;
     strcpy(dir, default_path);
-    int total = scandir(dir, &namelist, 0, alphasort);
+    int total = scandir(dir, &namelist, 0, alphasort); //查看所有文件的个数
     printf("path=%s, total = %d\n", dir, total);
+    for (int i = 0; i < total; i ++) {
+        strcpy(str, namelist[i]->d_name);
+        if (str[0] >= '0' && str[0] <= '9') count ++;
+    }
+    printf("进程数：%d\n", count);
 }
