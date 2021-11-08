20f86a1ffaad6524f305b0b60549b08ae3a146d9
tracer-nju <tracer@nju.edu.cn>
2021-11-08 15:51:16 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  15:51:16 up 122 days, 20:40,  0 users,  load average: 0.02, 0.06, 0.09

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,153 +1,3 @@
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <errno.h>
-// #include <string.h>
-// #include <netdb.h>
-// #include <sys/types.h>
-// #include <pthread.h>
-// #include <unistd.h>
-// #include <dirent.h>
-
-// char default_path[1024] = "/proc/";
-
-// typedef struct file_info{
-//     int pid;
-//     int ppid;
-//     char name[1024];
-//     int flag;
-//     int rec;
-// } info;
-
-// int my_getpid(char *str) {
-//     int len = strlen(str);
-//     char num[10];
-//     int i, j, ret;
-//     if (strncmp(str, "Pid", 3) == 0) {
-//         for (i = 0; i < len; i ++) {
-//             if (str[i] >= '0' && str[i] <= '9') break;
-//         }
-//         for (j = 0; j < len - i; j ++) {
-//             num[j] = str[i + j];
-//         }
-//         ret = atoi(num);
-//     } else {
-//         ret = 0;
-//     }
-//     return ret;
-// }
-
-// int my_getppid(char *str) {
-//     int len = strlen(str);
-//     char num[10];
-//     int i, j, ret;
-//     if (strncmp(str, "PPid", 3) == 0) {
-//         for (i = 0; i < len; i ++) {
-//             if (str[i] >= '0' && str[i] <= '9') break;
-//         }
-//         for (j = 0; j < len - i; j ++) {
-//             num[j] = str[i + j];
-//         }
-//         ret = atoi(num);
-//     } else {
-//         ret = 0;
-//     }
-//     return ret;
-// }
-
-// void print_pstree(info *file,int count,int ppid,int rec){
-//     int i,j,k;
-//     for(i=0;i<count;i++){
-//         if(file[i].flag==0&&file[i].ppid==ppid)
-//         {
-//             file[i].rec=rec+1;
-//             file[i].flag=1;
-//             for(k=0;k<rec;k++)
-//                 printf("      ");
-//             printf("[%d]%s\n",file[i].pid,file[i].name);
-//             print_pstree(file,count,file[i].pid,file[i].rec);
-//         }
-//     }
-// }
-
-// int main(int argc, char *argv[]) {
-//     int count = 0;
-//     char dir[1024], str[1024];
-//     struct dirent **namelist;
-//     strcpy(dir, default_path);
-//     int total = scandir(dir, &namelist, 0, alphasort); //查看所有文件的个数
-//     printf("path=%s, total = %d\n", dir, total);
-//     for (int i = 0; i < total; i ++) {
-//         strcpy(str, namelist[i]->d_name);
-//         if (str[0] >= '0' && str[0] <= '9') count ++; // 统计进程的个数（字符开头为数字的是进程文件夹）
-//     }
-//     printf("进程数：%d\n", count);
-    
-//     info file[1024];
-//     int i = 0, t = 0, s1, s2, j, k;
-//     while (i < total) {
-//         FILE * fp;
-//         char path[1024], name[1024];
-//         int pid = 0, ppid = 0;
-//         strcpy(str, namelist[i]->d_name);
-//         strcpy(path, default_path);
-//         if (str[0] >= '0' && str[0] <= '9') { // 判断是进程文件
-//             strcat(path, str);
-//             strcat(path, "/status");
-//             fp = fopen(path, "r");
-//             while (!feof(fp)) {
-//                 fgets(str, 1024, fp);
-//                 if ((s1 = my_getpid(str)) != 0) pid = s1;
-//                 if ((s2 = my_getppid(str)) != 0) ppid = s2;
-//                 if (strncmp(str, "Name", 4) == 0) {
-//                     for (j = 4; j < strlen(str); j ++) {
-//                         if (str[j] >= 'a' && str[j] <= 'z') break;
-//                     }
-//                     for (k = j; k < strlen(str); k ++) {
-//                         name[k - j] = str[k];
-//                     }
-//                     name[k - j + 1] = '\0';
-//                 }
-//                 file[t].pid = pid;
-//                 file[t].ppid = ppid;
-//                 strcpy(file[t].name, name);
-//             }
-//             fclose(fp);
-//             t ++;
-//         }
-//         i ++;
-//     }
-//     memset(&file->flag, 0, count);
-//     memset(&file->rec, 0, count);
-//     print_pstree(file, count, 0 , 0);
-// }
-
-
-// #include <stdio.h>
-// #include <assert.h>
-// #include <string.h>
-
-// char default_path[1024] = "/proc/";
-
-// int main(int argc, char *argv[]) {
-//   assert(!argv[argc]);
-//   if (argc != 2) {
-//     printf("argc = %d, Argument Number Error!\n", argc);
-//     return 0;
-//   }
-//   assert(argv[1]);
-//   // printf("argv = %s\n", argv[1]);
-//   if (strcmp(argv[1], "-V") == 0 || strcmp(argv[1], "--version") == 0) {
-//     printf("version\n");
-//   } else if (strcmp(argv[1], "-n") == 0 || strcmp(argv[1], "--numeric-sort") == 0) {
-//     printf("numeric-sort\n");
-//   } else if (strcmp(argv[1], "-p") == 0 || strcmp(argv[1], "--show-pids") == 0) {
-//     printf("show-pids\n");
-//   } else {
-//     printf("There Is No Such Argument Named \"%s\"\n", argv[1]);
-//   }
-//   return 0;
-// }
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
@@ -157,51 +7,53 @@
 #include <pthread.h>
 #include <unistd.h>
 #include <dirent.h>
-char default_path[1024]="/proc/";
-typedef struct file_info
-{
+
+char default_path[1024] = "/proc/";
+
+typedef struct file_info{
     int pid;
     int ppid;
     char name[1024];
     int flag;
     int rec;
-}info;
+} info;
 
-int my_getpid(char *str){
-    int len=strlen(str);
+int my_getpid(char *str) {
+    int len = strlen(str);
     char num[10];
-    int i,j,ret;
-    if(strncmp(str,"Pid",3)==0){
-        for(i=0;i<len;i++){
-            if(str[i]>='0'&&str[i]<='9')
-                break;
+    int i, j, ret;
+    if (strncmp(str, "Pid", 3) == 0) {
+        for (i = 0; i < len; i ++) {
+            if (str[i] >= '0' && str[i] <= '9') break;
         }
-        for(j=0;j<len-i;j++){
-            num[j]=str[i+j];
+        for (j = 0; j < len - i; j ++) {
+            num[j] = str[i + j];
         }
-        ret=atoi(num);
-    }else
-        ret=0;
+        ret = atoi(num);
+    } else {
+        ret = 0;
+    }
     return ret;
 }
-int my_getppid(char *str){
-    int len=strlen(str);
+
+int my_getppid(char *str) {
+    int len = strlen(str);
     char num[10];
-    int i,j,ret;
-    if(strncmp(str,"PPid",4)==0){
-        for(i=0;i<len;i++){
-            if(str[i]>='0'&&str[i]<='9')
-                break;
+    int i, j, ret;
+    if (strncmp(str, "PPid", 3) == 0) {
+        for (i = 0; i < len; i ++) {
+            if (str[i] >= '0' && str[i] <= '9') break;
         }
-        for(j=0;j<len-i;j++){
-            num[j]=str[i+j];
+        for (j = 0; j < len - i; j ++) {
+            num[j] = str[i + j];
         }
-        ret=atoi(num);
-        }
-    else
-        ret=0;
+        ret = atoi(num);
+    } else {
+        ret = 0;
+    }
     return ret;
 }
+
 void print_pstree(info *file,int count,int ppid,int rec){
     int i,j,k;
     for(i=0;i<count;i++){
@@ -216,58 +68,56 @@ void print_pstree(info *file,int count,int ppid,int rec){
         }
     }
 }
-int main(){
-    int i,j,k,total,s1,s2,count = 0,t;
-    char str[1024],dir[1024];
+
+int main(int argc, char *argv[]) {
+    int count = 0;
+    char dir[1024], str[1024];
     struct dirent **namelist;
-    strcpy(dir,default_path);
-    total = scandir(dir, &namelist, 0, alphasort);
-    printf("path=%s,total=%d\n",dir,total);
-    for(i=0;i<total;i++){
-        strcpy(str,namelist[i]->d_name);
-        if(str[0]>='0'&&str[0]<='9')
-            count++;
+    strcpy(dir, default_path);
+    int total = scandir(dir, &namelist, 0, alphasort); //查看所有文件的个数
+    printf("path=%s, total = %d\n", dir, total);
+    for (int i = 0; i < total; i ++) {
+        strcpy(str, namelist[i]->d_name);
+        if (str[0] >= '0' && str[0] <= '9') count ++; // 统计进程的个数（字符开头为数字的是进程文件夹）
     }
-    printf("进程数:%d\n",count);
+    printf("进程数：%d\n", count);
+    
     info file[1024];
-    i=0;t=0;
-    while(i<total)
-    {
-    FILE *fp;
-    char path[1024],name[1024];
-    int pid = 0, ppid = 0;
-    strcpy(str,namelist[i]->d_name);
-    strcpy(path,default_path);
-    if(str[0]>='0'&&str[0]<='9'){
-        strcat(path,str);
-        strcat(path,"/status");
-        fp=fopen(path,"r");
-        while(!feof(fp)){
-            fgets(str,1024,fp);
-                if((s1=my_getpid(str))!=0)
-                    pid=s1;
-                if((s2=my_getppid(str))!=0)
-                    ppid=s2;
-                if(strncmp(str,"Name",4)==0){
-                    for(j=4;j<strlen(str);j++){
-                        if(str[j]>='a'&&str[j]<='z')
-                            break;
+    int i = 0, t = 0, s1, s2, j, k;
+    while (i < total) {
+        FILE * fp;
+        char path[1024], name[1024];
+        int pid = 0, ppid = 0;
+        strcpy(str, namelist[i]->d_name);
+        strcpy(path, default_path);
+        if (str[0] >= '0' && str[0] <= '9') { // 判断是进程文件
+            strcat(path, str);
+            strcat(path, "/status");
+            fp = fopen(path, "r");
+            while (!feof(fp)) {
+                fgets(str, 1024, fp);
+                printf("%s\n", str);
+                if ((s1 = my_getpid(str)) != 0) pid = s1;
+                if ((s2 = my_getppid(str)) != 0) ppid = s2;
+                if (strncmp(str, "Name", 4) == 0) {
+                    for (j = 4; j < strlen(str); j ++) {
+                        if (str[j] >= 'a' && str[j] <= 'z') break;
                     }
-                    for(k=j;k<strlen(str);k++){
-                        name[k-j]=str[k];
+                    for (k = j; k < strlen(str); k ++) {
+                        name[k - j] = str[k];
                     }
-                    name[k-j-1]='\0';
+                    name[k - j + 1] = '\0';
                 }
-                file[t].pid=pid;
-                file[t].ppid=ppid;
-                strcpy(file[t].name,name);
+                file[t].pid = pid;
+                file[t].ppid = ppid;
+                strcpy(file[t].name, name);
             }
             fclose(fp);
-            t++;
+            t ++;
         }
-        i++;
+        i ++;
     }
-    memset(&file->flag,0,count);
-    memset(&file->rec,0,count);
-    print_pstree(file,count,0,0);
+    memset(&file->flag, 0, count);
+    memset(&file->rec, 0, count);
+    print_pstree(file, count, 0 , 0);
 }
