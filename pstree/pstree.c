5bc5aebcb8de38460a978ce56b5887b687e4135b
tracer-nju <tracer@nju.edu.cn>
2021-11-08 15:26:59 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  15:26:59 up 122 days, 20:15,  0 users,  load average: 0.13, 0.16, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -84,7 +84,7 @@ int main(int argc, char *argv[]) {
     printf("进程数：%d\n", count);
     
     info flie[1024];
-    int i = 0, t = 0, s1, s2;
+    int i = 0, t = 0, s1, s2, j, k;
     while (i < total) {
         FILE * fp;
         char path[1024], name[1024];
