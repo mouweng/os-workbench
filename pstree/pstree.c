70f3c64d1bbec986458140f9e41efac690e88273
tracer-nju <tracer@nju.edu.cn>
2021-11-08 15:26:32 +0800

    Static hostname: mouweng          Icon name: computer-vm            Chassis: vm         Machine ID: 7653b0a261254d8e81f87b3acd31b89c            Boot ID: 9e38b2828c254323be2433be08aee032     Virtualization: kvm   Operating System: CentOS Linux 7 (Core)        CPE OS Name: cpe:/o:centos:centos:7             Kernel: Linux 3.10.0-1160.31.1.el7.x86_64       Architecture: x86-64  15:26:32 up 122 days, 20:15,  0 users,  load average: 0.21, 0.18, 0.19

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -10,6 +10,65 @@
 
 char default_path[1024] = "/proc/";
 
+typedef struct file_info{
+    int pid;
+    int ppid;
+    char name[1024];
+    int flag;
+    int rec;
+} info;
+
+int my_getpid(char *str) {
+    int len = strlen(str);
+    char num[10];
+    int i, j, ret;
+    if (strncmp(str, "Pid", 3) == 0) {
+        for (i = 0; i < len; i ++) {
+            if (str[i] >= '0' && str[i] <= '9') break;
+        }
+        for (j = 0; j < len - i; j ++) {
+            num[j] = str[i + j];
+        }
+        ret = atoi(num);
+    } else {
+        ret = 0;
+    }
+    return ret;
+}
+
+int my_getppid(char *str) {
+    int len = strlen(str);
+    char num[10];
+    int i, j, ret;
+    if (strncmp(str, "PPid", 3) == 0) {
+        for (i = 0; i < len; i ++) {
+            if (str[i] >= '0' && str[i] <= '9') break;
+        }
+        for (j = 0; j < len - i; j ++) {
+            num[j] = str[i + j];
+        }
+        ret = atoi(num);
+    } else {
+        ret = 0;
+    }
+    return ret;
+}
+
+void print_pstree(info *file,int count,int ppid,int rec){
+    int i,j,k;
+    for(i=0;i<count;i++){
+        if(file[i].flag==0&&file[i].ppid==ppid)
+        {
+            file[i].rec=rec+1;
+            file[i].flag=1;
+            for(k=0;k<rec;k++)
+                printf("      ");
+            printf("[%d]%s\n",file[i].pid,file[i].name);
+            print_pstree(file,count,file[i].pid,file[i].rec);
+        }
+    }
+}
+
 int main(int argc, char *argv[]) {
     int count = 0;
     char dir[1024], str[1024];
@@ -20,7 +79,45 @@ int main(int argc, char *argv[]) {
     for (int i = 0; i < total; i ++) {
         strcpy(str, namelist[i]->d_name);
         printf("%s\n", str);
-        if (str[0] >= '0' && str[0] <= '9') count ++;
+        if (str[0] >= '0' && str[0] <= '9') count ++; // 统计进程的个数（字符开头为数字的是进程文件夹）
     }
     printf("进程数：%d\n", count);
+    
+    info flie[1024];
+    int i = 0, t = 0, s1, s2;
+    while (i < total) {
+        FILE * fp;
+        char path[1024], name[1024];
+        int pid, ppid;
+        strcpy(str, namelist[i]->d_name);
+        strcpy(path, default_path);
+        if (str[0] >= '0' && str[0] <= '9') { // 判断是进程文件
+            strcat(path, str);
+            strcat(path, "/status");
+            fp = fopen(path, "r");
+            while (!feof(fp)) {
+                fgets(str, 1024, fp);
+                if ((s1 = my_getpid(str)) != 0) pid = s1;
+                if ((s2 = my_getppid(str)) != 0) ppid = s2;
+                if (strncmp(str, "Name", 4) == 0) {
+                    for (j = 4; j < strlen(str); j ++) {
+                        if (str[j] >= 'a' && str[j] <= 'z') break;
+                    }
+                    for (k = j; k < strlen(str); k ++) {
+                        name[k - j] = str[k];
+                    }
+                    name[k - j + 1] = '\0';
+                }
+                file[t].pid = pid;
+                file[t].ppid = ppid;
+                strcpy(file[t].name, name);
+            }
+            fclose(fp);
+            t ++;
+        }
+        i ++;
+    }
+    memset(&file->flag, 0, count);
+    memset(&file->rec, 0, count);
+    print_pstree(file, count, 0 , 0);
 }
